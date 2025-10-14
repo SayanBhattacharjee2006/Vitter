c4fa18650aae4f8761b1989d8873c79c1037ceaf
sayan bhattacharjee <sayanbhattacharya0001@gmail.com>
2025-10-14 17:23:11 +0530

    mongodb connection set up

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// importing dotenv for environment variables
+import dotenv from 'dotenv';
+//configuring dotenv to load environment variables from a.env file in the root directory
+dotenv.config();
+
+import connectDB from "./db/index.js";
+
+// Connect to MongoDB
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// an method to connect to MongoDB
+
+
+// import express from 'express'
+// const app = express()
+
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",()=>{
+//             console.log("Error connecting to MongoDB: ", err)
+//             process.exit(1);
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server running on port ${process.env.PORT}`)
+//         })
+//     }
+//     catch(err){
+//         console.log("Error connecting to MongoDB: ", err)
+//         throw err;
+//     }
+// })()
+
