42293095707c4f29ac260d9fc9cb29deeee6b277
sayan bhattacharjee <sayanbhattacharya0001@gmail.com>
2025-10-23 20:14:33 +0530

    feat: implement video publishing and fetching functionality with validation and error handling

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,6 +5,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { updateAccountDetails } from "./user.controller.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -13,13 +14,125 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    const { title, description} = req.body
+    const user = req.user
+
+    if(!title || !description ){
+        throw new ApiError(400,"Title and description required")
+    }
+
+    if(title.length>30){
+        throw new ApiError(400,"Title must be within 30 characters")
+    }
+
+    if(description.length>1000){
+        throw new ApiError(400,"Description must be within 1000 characters")
+    }
+
+    const videoFilePath = req.files?.videoFile?.[0]?.path
+    const thumbnailPath = req.files?.thumbnail?.[0]?.path
+
+    if(!videoFilePath){
+        throw new ApiError(400,"video file is required")
+    }
+
+    if(!thumbnailPath){
+        throw new ApiError(400,"thumbnail file is required")
+    }
+    
+    const uploadedVideo = await uploadOnCloudinary(videoFilePath,"video")
+    const uploadedThumbnail = await uploadOnCloudinary(thumbnailPath,"image")
+    
+    if(!uploadedVideo?.url){
+        throw new ApiError(500,"Something went wrong while uploading video to cloudinary")
+    }
+
+    if(!uploadedThumbnail?.url){
+        throw new ApiError(500,"Something went wrong while uploading thumbnail to cloudinary")
+    }
+ 
+
+    const newPublishedVideo = await Video.create({
+        videoFile:uploadedVideo.url,
+        thumbnail:uploadedThumbnail.url,
+        title,
+        description,
+        owner:user._id,
+        duration:uploadedVideo.duration,
+        isPublished:true
+    })
+
+    if(!newPublishedVideo){
+        throw new ApiError(500,"Something went wrong while publishing video ")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            newPublishedVideo,
+            "Video successfully published"
+        )
+    )
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
     //TODO: get video by id
+    
+    const { videoId } = req.params
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid video Id ")
+    }
+
+    const video = await Video.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullName:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                owner:{
+                    $first:"$owner"
+                }
+            }
+        }
+    ])
+
+    if(!video.length){
+        throw new ApiError(404,"Video not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            video[0],
+            "Video successfully fetched"
+        )
+    )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -8,10 +8,37 @@ import {
     togglePublishStatus
 } from '../controllers/subscription.controller';
 import { verifyJWT } from '../middlewares/auth.middleware';
+import { upload } from "../middlewares/multer.middleware"
 
 const router = Router();
 
 router.use(verifyJWT);
 
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        upload.fields([
+            {
+                name:"videoFile",
+                maxCount:1
+            },
+            {
+                name:"thumbnail",
+                maxCount:1
+            }
+        ]),
+        publishAVideo
+    )
+
+router
+    .route("/:videoId")
+    .get(getVideoById)
+    .patch(updateVideo)
+    .delete(upload.single("thumbnail"),deleteVideo)
+
+router
+    .route("/toggle/publish/:videoId")
+    .patch(togglePublishStatus)
 
 export default router;
